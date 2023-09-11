0a5ec37ed80557a99053bdaba073d9bb3e7079e6
Kauan <kauan.barbosa@aluno.ifsp.edu.br>
2023-09-11 09:29:29 -0300

    post adicionado

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const connection = mysql.createPool({
 })
 
 app.get('/', (req,res)=>{
-    res.send("Eita pimbas?");
+    res.send("Kauan Barbosa Machado");
 })
 
 const getAllPessoas = async () =>{
@@ -24,4 +24,17 @@ const getAllPessoas = async () =>{
 app.get('/pessoa', async (req,res)=>{
     const consulta = await getAllPessoas()
     return res.status(200).json(consulta)
+})
+
+app.get('/pessoa/:id', async (req,res)=>{
+    const {id} = req.params;
+    const [query] = await connection.execute('select * from TestePessoa.Pessoa where id = ?', [id]);
+    if(query.lenght === 0) return res.status(400).json({mensagem: 'Nao encontrado. '})
+    return res.status(200).json(query);
+})
+
+app.post('/pessoa', async (req,res)=>{
+    const {nome, email} = req.body
+    const [query] = await connection.execute('insert into TestePessoa.Pessoa (nome,email) values(?,?)', [nome,email])
+    return res.status(200).json(query)
 })
