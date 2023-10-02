8998c6cc06fe86045fc59af824c7bb97086ef4a9
Rafael Ribeiro <klkp.398@gmail.com>
2023-10-02 09:24:26 -0300

    Feito métodos get,put,delete para Doador, Usuário, e Doação
    Co-authored-by: dai <daiuan@users.noreply.github.com>

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,7 @@ const connection = mysql.createPool({
     password: ''
 })
 
+/*
 app.get('/', (req,res)=>{
     res.send("Kauan Barbosa Machado");
 })
@@ -36,7 +37,8 @@ app.get('/pessoa/:id', async (req,res)=>{
 
 app.get('/pessoa/buscar/:nome', async (req,res)=>{
     const {nome} = req.params;
-    const [query] = await connection.execute('select * from TestePessoa.Pessoa where nome = ?', [nome]);;
+    let nomex = '%'+nome+'%'
+    const [query] = await connection.execute('select * from TestePessoa.Pessoa where nome like ?', [nomex]);
     if(query.lenght === 0) return res.status(400).json({mensagem: 'Nao encontrado. '});
     return res.status(200).json(query);
 })
@@ -51,7 +53,7 @@ app.post('/pessoa', async (req,res)=>{
 app.put('/pessoa/:id', async (req, res) => {
     const { id } = req.params;
     const {nome, email} = req.body;
-    const [query] = await connection.execute('UPDATE TestePessoa.Pessoa SET nome = ?, email = ? WHERE id = ?', [nome, email, id]);
+    const [query] = await connection.execute('UPDATE TestePessoa.Pessoa SET nome = ?, email = ? WHERE id = ?', [id, nome, email]);
     if (query.affectedRows === 0) return res.status(404).json({ mensagem: 'Pessoa não encontrada.' });
     return res.status(200).json({ mensagem: 'Pessoa alterada com sucesso.' });
 })
@@ -61,6 +63,138 @@ app.delete('/pessoa/:id', async (req,res)=>{
     const [query] = await connection.execute('delete from TestePessoa.Pessoa where id = ?', [id]);
     if (query.affectedRows === 0) return res.status(404).json({ mensagem: 'Não encontrado.' });
     return res.status(200).json({ mensagem: 'Pessoa excluída com sucesso.' });
+}) */
+
+
+// MÉTODOS PARA A TABELA DOADOR 
+
+app.get('/', (req,res)=>{
+    res.send("Kauan Barbosa Machado");
 })
 
+const getAlldoador = async () =>{
+    const [query] = await connection
+    .execute ('select * from karadoa.doador');
+    return query;
+}
 
+app.get('/doador', async (req,res)=>{
+    const consulta = await getAlldoador();
+    return res.status(200).json(consulta);
+})
+
+app.get('/doador/:id', async (req,res)=>{
+    const {id} = req.params;
+    const [query] = await connection.execute('select * from karadoa.doador where id = ?', [id]);
+    if(query.lenght === 0) return res.status(400).json({mensagem: 'Nao encontrado. '});
+    return res.status(200).json(query);
+})
+
+app.get('/doador/buscar/:nome', async (req,res)=>{
+    const {nome} = req.params;
+    let nomex = '%'+nome+'%'
+    const [query] = await connection.execute('select * from karadoa.doador where nome like ?', [nomex]);
+    if(query.lenght === 0) return res.status(400).json({mensagem: 'Nao encontrado. '});
+    return res.status(200).json(query);
+})
+
+app.post('/doador', async (req,res)=>{
+    const {nome, email, cpf, telefone, senha, cep, cidade, estado} = req.body;
+    const [query] = await connection.execute('insert into karadoa.doador (nome,email,cpf,telefone,senha,cep,cidade,estado) values(?,?,?,?,?,?,?,?)', [nome,email,cpf,telefone,senha,cep,cidade,estado]);
+    if(query.lenght === 0) return res.status(400).json({mensagem: 'Erro na adição'});
+    return res.status(200).json({ mensagem: 'Inserido com sucesso.' });
+})
+
+app.put('/doador/:id', async (req, res) => {
+    const { id } = req.params;
+    const {nome, email} = req.body;
+    const [query] = await connection.execute('UPDATE karadoa.doador SET nome = ?, email = ? WHERE id = ?', [nome, email, id]);
+    if (query.affectedRows === 0) return res.status(404).json({ mensagem: 'Doador não encontrado.' });
+    return res.status(200).json({ mensagem: 'Doador alterado com sucesso.' });
+})
+
+app.delete('/doador/:id', async (req,res)=>{
+    const { id } = req.params;
+    const [query] = await connection.execute('delete from karadoa.doador where id = ?', [id]);
+    if (query.affectedRows === 0) return res.status(404).json({ mensagem: 'Não encontrado.' });
+    return res.status(200).json({ mensagem: 'Doador excluído com sucesso.' });
+})
+
+// MÉTODOS PARA A TABELA DOAÇÃO
+
+const getAlldoacao = async () =>{
+    const [query] = await connection
+    .execute ('select * from karadoa.doacao');
+    return query;
+}
+
+app.get('/doacao', async (req,res)=>{
+    const consulta = await getAlldoacao();
+    return res.status(200).json(consulta);
+})
+
+app.get('/doacao/:id', async (req,res)=>{
+    const {id} = req.params;
+    const [query] = await connection.execute('select * from karadoa.doacao where id = ?', [id]);
+    if(query.lenght === 0) return res.status(400).json({mensagem: 'Nao encontrado. '});
+    return res.status(200).json(query);
+})
+
+app.post('/doacao', async (req,res)=>{
+    const {id_doador, id_campanha, valor, status, data, modelo, mensagem} = req.body;
+    const [query] = await connection.execute('insert into karadoa.docao (id_doador,id_campanha,valor,status,data,modelo,mensagem) values(?,?,?,?,?,?,?)', [id_doador,id_campanha,valor,status,data,modelo,mensagem]);
+    if(query.lenght === 0) return res.status(400).json({mensagem: 'Erro na adição'});
+    return res.status(200).json({ mensagem: 'Inserido com sucesso.' });
+})
+
+// MÉTODOS PARA A TABELA CAMPANHA
+
+// MÉTODOS PARA A TABELA USUÁRIO
+
+const getAllusuario = async () =>{
+    const [query] = await connection
+    .execute ('select * from karadoa.usuario');
+    return query;
+}
+
+app.get('/usuario', async (req,res)=>{
+    const consulta = await getAllusuario();
+    return res.status(200).json(consulta);
+})
+
+app.get('/usuario/:id', async (req,res)=>{
+    const {id} = req.params;
+    const [query] = await connection.execute('select * from karadoa.usuario where id = ?', [id]);
+    if(query.lenght === 0) return res.status(400).json({mensagem: 'Nao encontrado. '});
+    return res.status(200).json(query);
+})
+
+app.get('/usuario/buscar/:nome', async (req,res)=>{
+    const {nome} = req.params;
+    let nomex = '%'+nome+'%'
+    const [query] = await connection.execute('select * from karadoa.usuario where nome like ?', [nomex]);
+    if(query.lenght === 0) return res.status(400).json({mensagem: 'Nao encontrado. '});
+    return res.status(200).json(query);
+})
+
+app.post('/usuario', async (req,res)=>{
+    const {nome, email, cpf, telefone, senha, cep, cidade, estado} = req.body;
+    const [query] = await connection.execute('insert into karadoa.usuario (nome,email,cpf,telefone,senha,cep,cidade,estado) values(?,?,?,?,?,?,?,?)', [nome,email,cpf,telefone,senha,cep,cidade,estado]);
+    if(query.lenght === 0) return res.status(400).json({mensagem: 'Erro na adição'});
+    return res.status(200).json({ mensagem: 'Inserido com sucesso.' });
+})
+
+app.put('/usuario/:id', async (req, res) => {
+    const { id } = req.params;
+    const {nome, email} = req.body;
+    const [query] = await connection.execute('UPDATE karadoa.usuario SET nome = ?, email = ? WHERE id = ?', [nome, email, id]);
+    if (query.affectedRows === 0) return res.status(404).json({ mensagem: 'Usuário não encontrado.' });
+    return res.status(200).json({ mensagem: 'Usuário alterado com sucesso.' });
+})
+
+app.delete('/usuario/:id', async (req,res)=>{
+    const { id } = req.params;
+    const [query] = await connection.execute('delete from karadoa.usuario where id = ?', [id]);
+    if (query.affectedRows === 0) return res.status(404).json({ mensagem: 'Não encontrado.' });
+    return res.status(200).json({ mensagem: 'Usuário excluído com sucesso.' });
+})
